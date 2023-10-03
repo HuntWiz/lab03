14276ab7a4a92080f4a2e2387500a20c8dfea09c
Hunt <botalov.gleb123@gmail.com>
2023-10-03 18:21:25 +0500

    Сортировка в обе стороны

--- main.py
+++ main.py
@@ -7,9 +7,18 @@ for i in range(N):
     a.append(int(input()))
 print(a)
 
-for i in range(N - 1):
-    for j in range(N - i - 1):
-        if a[j] > a[j + 1]:
-            a[j], a[j + 1] = a[j + 1], a[j]
+print("В каком порядке сортировать: 0 - Возрастание, 1 - Убывание")
+var = int(input())
+
+if(var == 0):
+    for i in range(N - 1):
+        for j in range(N - i - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+else:
+    for i in range(N - 1):
+        for j in range(N - i - 1):
+            if a[j] < a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
 
 print(a)
